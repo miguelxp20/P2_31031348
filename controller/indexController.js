8940df2de88f7d191c8462c0d86f171ed62ef81f
Miguel <miguelxponce22@gmail.com>
2024-05-20 13:50:42 -0400

    Nuevo proyecto

--- controller/indexController.js
+++
@@ -1,8 +0,0 @@
-const toIndex = (req, res, next)=> {
-    res.render('index', { 
-      title: 'Express Formulario',
-      title2: "Pagina de confirmacion"
-    });
-  };
-
-  module.exports = toIndex;
