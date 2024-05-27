2a3320c7d910f01ed27283b7aacfb06dfd203df6
Miguel <miguelxponce22@gmail.com>
2024-05-27 12:32:48 -0400

    Modificaciones2

--- controller/ContactosController.js
+++ controller/ContactosController.js
@@ -6,6 +6,28 @@ class ContactosController {
         this.add = this.add.bind(this);
     }
 
+    async obtenerIp() {
+      try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        return data.ip; // Retorna la ip
+      } catch (error) {
+        console.error('Error al obtener la ip:', error);
+        return null; // Retorna null si hay un error
+      }
+    }
+  
+    async obtenerPais(ip) {
+      try {
+        const response = await fetch('https://ipinfo.io/'+ip+'?token=197c576e48e0c3');
+        const data = await response.json();
+        return data.country; // Retorna el país
+      } catch (error) {
+        console.error('Error al obtener el país:', error);
+        return null; // Retorna null si hay un error
+      }
+    }
+
     async add(req, res){
         // Validar los datos del formulario
         const { name, email, comment } = req.body;
@@ -17,12 +39,13 @@ class ContactosController {
 
 
         //Guardar los datos del formulario
-        const ip = req.ip;
+        const ip = await this.obtenerIp();
         const fecha = new Date().toISOString();
+        const pais = await this.obtenerPais(ip);
         
 
 
-        await this.contactosModel.crearContacto(name, email, comment, ip, fecha);
+        await this.contactosModel.crearContacto(name, email, comment, ip, fecha, pais);
 
         const contactos = await this.contactosModel.obtenerAllContactos();
     
